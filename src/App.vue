5ae9f9c9bbc17f3b6f452cb6b1adf5667b41997a
Karem Cristini de Souza <56806818+KSouzaEng@users.noreply.github.com>
2021-04-02 18:43:33 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,145 @@
+<template>
+  <div id="app">
+    <img class="img" src="./assets/cronometro.png" />
+    <a class="timer">{{ numero }}</a>
+    <div class="area-btn">
+      <button class="botao" @click="vai">{{ botao }}</button>
+      <button class="botao" @click="limpar">VOLTAR</button>
+    </div>
+    <div class="list" v-show="historico.length > 0">
+      <ul>
+        <li v-for="item in historico" :key="item">
+          VOCÊ FEZ UMA PAUSA EM:{{ item }}
+        </li>
+      </ul>
+      <button @click="historico = []">LIMPAR HISTORICO</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      numero: 0,
+      botao: "VAI",
+      timer: null,
+      ss: 0,
+      mm: 0,
+      hh: 0,
+      historico: [],
+    };
+  },
+  methods: {
+    vai() {
+      if (this.timer !== null) {
+        clearInterval(this.timer);
+        this.timer = null;
+        this.botao = "VAI";
+        if (this.ss !== 0) {
+          this.historico.push(this.numero);
+        }
+      } else {
+        this.timer = setInterval(() => {
+          this.rodarTime();
+        }, 400);
+        this.botao = "PAUSAR";
+      }
+    },
+    limpar() {
+      if (this.timer !== null) {
+        clearInterval(this.timer);
+        this.timer = null;
+      }
+      this.ss = 0;
+      this.mm = 0;
+      this.hh = 0;
+      this.numero = 0;
+      this.botao = "VAI";
+      this.historico = [];
+    },
+    rodarTime() {
+      this.ss++;
+      if (this.ss == 59) {
+        this.ss = 0;
+        this.mm++;
+      }
+      if (this.mm == 59) {
+        this.mm = 0;
+        this.hh++;
+      }
+      let format =
+        (this.hh < 10 ? "0" + this.hh : this.hh) +
+        ":" +
+        (this.mm < 10 ? "0" + this.mm : this.mm) +
+        "," +
+        (this.ss < 10 ? "0" + this.ss : this.ss);
+
+      return (this.numero = format);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+.img {
+  width: 420px;
+  height: 100%;
+  padding-top: 100px;
+}
+.timer {
+  color: #ffff;
+  font-size: 70px;
+  margin-top: -210px;
+}
+.area-btn {
+  margin-top: 155px;
+  display: flex;
+}
+.botao {
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  width: 150px;
+  background-color: #FFF;
+  font-size: 20px;
+  border: 0;
+  border-radius: 5px;
+  text-align: center;
+  margin-left: 15px;
+  margin-right: 15px;
+  padding: 6px;
+  cursor: pointer;
+}
+.botao:hover {
+  opacity: 0.8;
+  trasition: all 0.5s;
+}
+ul {
+  text-align: center;
+  padding: 15px;
+}
+ul li {
+  margin-top: 4px;
+  padding: 15px;
+  background-color: rgb(70, 70, 70);
+  list-style: none;
+  font-size: 18px;
+  border-radius: 6px;
+}
+.list button {
+  cursor: pointer;
+  border: 0;
+  background-color: #FFF;
+  padding: 8px;
+  border-radius: 5px;
+  margin-bottom: 12px;
+}
+</style>
